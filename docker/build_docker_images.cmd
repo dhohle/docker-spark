fc9fc2e771ffbb27bad4dae534d6f88d6a0372e7
Daniël Höhle <daniel.hohle@gmail.com>
2021-05-19 15:36:09 +0200

    local dockerfile updates

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -1,3 +1,7 @@
-docker build -f Dockerfile_master -t sdesilva26/spark_master -t sdesilva26/spark_master:0.0.2 .
-docker build -f Dockerfile_worker -t sdesilva26/spark_worker -t sdesilva26/spark_worker:0.0.2 .
-docker build -f Dockerfile_submit -t sdesilva26/spark_submit -t sdesilva26/spark_submit:0.0.2 .
+@REM docker build -f Dockerfile_master -t harriedekorte/spark_master -t harriedekorte/spark_master:0.0.2 .
+@REM docker build -f Dockerfile_worker -t harriedekorte/spark_worker -t harriedekorte/spark_worker:0.0.2 .
+@REM docker build -f Dockerfile_submit -t harriedekorte/spark_submit -t harriedekorte/spark_submit:0.0.2 .
+docker build -f Dockerfile_base -t harriedekorte/spark_base -t harriedekorte/spark_base:latest .
+docker build -f Dockerfile_master -t harriedekorte/spark_master -t harriedekorte/spark_master:latest .
+docker build -f Dockerfile_worker -t harriedekorte/spark_worker -t harriedekorte/spark_worker:latest .
+docker build -f Dockerfile_submit -t harriedekorte/spark_submit -t harriedekorte/spark_submit:latest .
