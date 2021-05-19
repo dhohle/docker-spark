b901b585e1dbd1df5df324fc20a8141de3a050b5
Daniël Höhle <daniel.hohle@gmail.com>
2021-05-19 15:50:48 +0200

    minor

--- docker/build_docker_images.cmd
+++ docker/build_docker_images.cmd
@@ -1,6 +1,6 @@
-@REM docker build -f Dockerfile_master -t harriedekorte/spark_master -t harriedekorte/spark_master:0.0.2 .
-@REM docker build -f Dockerfile_worker -t harriedekorte/spark_worker -t harriedekorte/spark_worker:0.0.2 .
-@REM docker build -f Dockerfile_submit -t harriedekorte/spark_submit -t harriedekorte/spark_submit:0.0.2 .
+#REM docker build -f Dockerfile_master -t harriedekorte/spark_master -t harriedekorte/spark_master:0.0.2 .
+#REM docker build -f Dockerfile_worker -t harriedekorte/spark_worker -t harriedekorte/spark_worker:0.0.2 .
+#REM docker build -f Dockerfile_submit -t harriedekorte/spark_submit -t harriedekorte/spark_submit:0.0.2 .
 docker build -f Dockerfile_base -t harriedekorte/spark_base -t harriedekorte/spark_base:latest .
 docker build -f Dockerfile_master -t harriedekorte/spark_master -t harriedekorte/spark_master:latest .
 docker build -f Dockerfile_worker -t harriedekorte/spark_worker -t harriedekorte/spark_worker:latest .
